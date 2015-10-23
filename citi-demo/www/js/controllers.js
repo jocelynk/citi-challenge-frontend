f0628cd0479d39e27f76cfcebcd0d5e0d3a1ecc9
jocelynk <jocey.kong@gmail.com>
2015-10-23 15:01:42 -0400

    adding bluetooth

--- citi-demo/www/js/controllers.js
+++ citi-demo/www/js/controllers.js
@@ -1,9 +1,32 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['starter.services', 'ngCordova', 'ngCordovaBeacon'])
 
-  .controller('DashCtrl', function ($scope) {
+  .controller('DashCtrl', function ($scope, $ionicPlatform) {
+     var server = new WebSocket("ws://10.128.16.213:9000/ws");
+    $scope.account = {};
+    $scope.showAccount = false;
+    server.onopen = function (event) {
+      server.send("Message to send");
+      console.log(event);
+
+    };
+
+    server.onmessage = function (event) {
+      var account = event.data;
+      if(account.userId) {
+        $scope.account.userId = account.userId;
+        $scope.showAccount = true;
+        $scope.$apply();
+      }
+    };
+
+    server.onclose = function(event) {
+      console.log(event)
+    };
+
+
   })
 
-  .controller('ChatsCtrl', function ($scope, Chats) {
+  .controller('ChatsCtrl', function ($scope, Chats, $ionicPlatform) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -16,34 +39,192 @@ angular.module('starter.controllers', [])
     $scope.remove = function (chat) {
       Chats.remove(chat);
     };
+
+    $scope.bindCordovaEvents = function () {
+      document.addEventListener('bcready', $scope.onBCReady, false);
+    };
+
+    $scope.onNewIBeacon = function (s) {
+      var newibeacon = s.target;
+      console.log(newibeacon);
+    };
+
+    $scope.onBCReady = function () {
+      BC.bluetooth.addEventListener("bluetoothstatechange", $scope.onBluetoothStateChange);
+      BC.bluetooth.addEventListener("newdevice", $scope.addNewDevice);
+      BC.iBeaconManager.addEventListener("newibeacon", $scope.onNewIBeacon);
+      if (!BC.bluetooth.isopen) {
+        if (API !== "ios") {
+          BC.Bluetooth.OpenBluetooth(function () {
+          });
+        } else {
+          //alert("Please open your bluetooth first.");
+        }
+      }
+    };
+
+    $scope.onBluetoothStateChange = function () {
+      if (BC.bluetooth.isopen) {
+
+      } else {
+        if (API !== "ios") {
+          BC.Bluetooth.OpenBluetooth(function () {
+          });
+        } else {
+          //alert("Please open your bluetooth first.");
+        }
+      }
+    };
+
+    $scope.onBluetoothDisconnect = function (arg) {
+      BC.Proximity.clearPathLoss();
+      navigator.notification.stopBeep();
+
+    };
+
+    $scope.onDeviceConnected = function (arg) {
+      var deviceAddress = arg.deviceAddress;
+
+    };
+
+    $scope.addNewDevice = function (s) {
+      var newDevice = s.target;
+      newDevice.addEventListener("deviceconnected", $scope.onDeviceConnected);
+      newDevice.addEventListener("devicedisconnected", $scope.onBluetoothDisconnect);
+      console.log(s);
+
+    };
+
+    $scope.onDeviceDisconnected = function () {
+      BC.Proximity.clearPathLoss();
+      navigator.notification.stopBeep();
+    };
+
+    $scope.manuallyAddNewDevice = function (address) {
+      var device = BC.bluetooth.devices[address];
+      if(device) {
+        device.addEventListener("deviceconnected", function (s) {
+          console.log("device:" + s.deviceAddress + "is connected successfully!")
+        });
+        device.addEventListener("devicedisconnected", function (s) {
+          console.log("device:" + s.deviceAddress + "is connected successfully!")
+        });
+        device.connect(function () {
+          console.log("device is already connected well!");
+        });
+        //device.connect(function(){alert("device is already connected well!");},null,"7A9C3B55-78D0-44A7-A94E-A93E3FE118CE",ture); //connect if the device is classical
+
+      }
+    };
+
+    $ionicPlatform.ready(function () {
+
+      $scope.bindCordovaEvents();
+      BC.Bluetooth.StopScan();
+      BC.Bluetooth.StartScan();
+
+      BC.Bluetooth.GetPairedDevices(function (mes) {
+        $scope.manuallyAddNewDevice(mes.deviceAddress);
+      });
+
+
+      /*      BC.Bluetooth.GetConnectedDevices(function(mes){
+       $scope.manuallyAddNewDevice(mes.deviceAddress);
+       });*/
+
+
+    });
+
+
   })
 
   .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
   })
 
-  .controller('AccountCtrl', function ($scope, $ionicPlatform, $cordovaDevice, $cordovaBarcodeScanner) {
-    $scope.settings = {
-      enableFriends: true
-    };
+  .controller('AccountCtrl', function ($rootScope, $scope, $ionicPlatform, $cordovaDevice, $cordovaBarcodeScanner, $cordovaBeacon, DeviceRegistration, BluetoothDiscovery) {
+
     $scope.uuid = null;
+    $scope.showDeviceList = false;
+    $scope.devices = [];
+
+    /*var brIdentifier = 'estimote';
+     var brUuid = 'C008F6A9-7CC8-A9B8-995F-D93069032173';
+     var brMajor = null;
+     var brMinor = null;
+     var brNotifyEntryStateOnDisplay = true;*/
+
 
     $ionicPlatform.ready(function () {
       if (window.cordova) {
 
         $scope.uuid = $cordovaDevice.getUUID();
-      }
 
+        BluetoothDiscovery.bindCordovaEvents();
+        BluetoothDiscovery.stopScan();
+        BluetoothDiscovery.startScan();
+
+      }
     });
 
-    $cordovaBarcodeScanner
-      .scan()
-      .then(function(barcodeData) {
-        console.log(barcodeData);
-        // Success! Barcode data is here
-      }, function(error) {
-        // An error occurred
+    $scope.scan = function () {
+      if (window.cordova) {
+        $cordovaBarcodeScanner
+          .scan()
+          .then(function (barcodeData) {
+            //"{userName: Bob, primeKey: 12345}"
+            var deviceInfo = {};
+            deviceInfo.userId = barcodeData.primeKey || '562992e0ca9ecba67aa0f95d';
+            deviceInfo.devices = [];
+
+            BC.Bluetooth.GetPairedDevices(function (mes) {
+              for (var i = 0; i < mes.length; i++) {
+                var d = {};
+                d.deviceId = mes[i].deviceAddress;
+                d.name = mes[i].deviceName || 'beacon';
+                deviceInfo.devices.push(d);
+                //BluetoothDiscovery.manuallyAddNewDevice(mes[i].deviceAddress);
+              }
+              $scope.devices = deviceInfo.devices;
+              $scope.showDeviceList = true;
+              console.log($scope.devices);
+              $scope.$apply();
+              DeviceRegistration.saveDeviceInfo(deviceInfo).then(function (success) {
+                console.log(success);
+                return success;
+              }, function (err) {
+                console.log(err);
+                return null;
+              });
+            });
+
+
+            /* DeviceRegistration.saveDeviceInfo(deviceInfo).then(function(success) {
+             console.log(success);
+             return success;
+             }, function(err) {
+             console.log(err);
+             return null;
+             });*/
+            // Success! Barcode data is here
+          }, function (error) {
+            // An error occurred
+          });
+      } else {
+        console.log("web browser doesn't support scanner");
+      }
+    };
+
+    $scope.getBluetoothDevices = function () {
+      $cordovaBeacon.requestWhenInUseAuthorization();
+
+      $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function (event, pluginResult) {
+        var uniqueBeaconKey;
+        for (var i = 0; i < pluginResult.beacons.length; i++) {
+          uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
+          $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+        }
+        $scope.$apply();
       });
-
-
+    };
   });
--- citi-demo/www/js/services.js
+++ citi-demo/www/js/services.js
@@ -1,50 +1,168 @@
 angular.module('starter.services', [])
-  .factory('')
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('SocketServer', function() {
+    var SocketServer = {};
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    SocketServer.server = new WebSocket("ws://10.128.16.213:9000/ws");
+    SocketServer.server.onopen = function (event) {
+      SocketServer.server.send("Message to send");
+      console.log(event);
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    };
+
+    SocketServer.server.onmessage = function (event) {
+      console.log(event);
+
+    };
+
+    SocketServer.server.onclose = function(event) {
+      console.log(event)
+    };
+
+    return SocketServer;
+
+  })
+  .factory('DeviceRegistration', function ($http) {
+    var DeviceRegistration = {};
+    DeviceRegistration.saveDeviceInfo = function (deviceInfo) {
+      return $http.post('http://10.128.16.213:9000/api/device', deviceInfo);
+
+    };
+
+    return DeviceRegistration;
+
+  })
+  .factory('BluetoothDiscovery', function ($http) {
+    var BluetoothDiscovery = {};
+    BluetoothDiscovery.devices = [];
+
+    BluetoothDiscovery.bindCordovaEvents = function() {
+      document.addEventListener('bcready', BluetoothDiscovery.onBCReady, false);
+    };
+
+
+    BluetoothDiscovery.onBCReady = function() {
+      BC.bluetooth.addEventListener("bluetoothstatechange",BluetoothDiscovery.onBluetoothStateChange);
+      BC.bluetooth.addEventListener("newdevice",BluetoothDiscovery.addNewDevice);
+    };
+
+    BluetoothDiscovery.onBluetoothStateChange = function(){
+      if(BC.bluetooth.isopen){
+
+      }else{
+        if(API !== "ios"){
+          BC.Bluetooth.OpenBluetooth(function(){
+          });
+        }else{
+          //alert("Please open your bluetooth first.");
         }
       }
-      return null;
-    }
-  };
-});
+    };
+
+    BluetoothDiscovery.onBluetoothDisconnect = function(arg){
+      BC.Proximity.clearPathLoss();
+      navigator.notification.stopBeep();
+
+    };
+
+    BluetoothDiscovery.onDeviceConnected = function(arg, callback){
+      var deviceAddress = arg.deviceAddress;
+      //if Device connected, logic to send to serve goes here.
+
+    };
+
+    BluetoothDiscovery.addNewDevice = function(s){
+      var newDevice = s.target;
+      newDevice.addEventListener("deviceconnected",BluetoothDiscovery.onDeviceConnected);
+      newDevice.addEventListener("devicedisconnected",BluetoothDiscovery.onBluetoothDisconnect);
+      console.log(s);
+
+    };
+
+    BluetoothDiscovery.onDeviceDisconnected = function(){
+      BC.Proximity.clearPathLoss();
+      navigator.notification.stopBeep();
+    };
+
+    BluetoothDiscovery.manuallyAddNewDevice = function(address, callback) {
+      var device = BC.bluetooth.devices[address];
+      if(device) {
+        device.addEventListener("deviceconnected", function (s) {
+          console.log("device:" + s.deviceAddress + "is connected successfully!")
+          //logic to send to server goes here
+        });
+
+        device.addEventListener("devicedisconnected", function (s) {
+          console.log("device:" + s.deviceAddress + "is connected successfully!")
+        });
+        device.connect(function (success) {
+          console.log(success);
+          console.log(device);
+          BluetoothDiscovery.devices.push({deviceId: device.deviceAddress});
+        }, function (err) {
+          console.log(err);
+          console.log(device);
+          console.log("Error connecting");
+        });
+      }
+    };
+
+    BluetoothDiscovery.startScan = function() {
+      BC.Bluetooth.StartScan();
+    };
+
+    BluetoothDiscovery.stopScan = function() {
+      BC.Bluetooth.StopScan();
+    };
+
+
+    return BluetoothDiscovery;
+
+  })
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
